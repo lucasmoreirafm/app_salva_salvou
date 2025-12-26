4fcc0af7954c191042a94faca113ca23ab9ed336
lucasmoreirafm <lucasmoreirafm@gmail.com>
2025-12-26 15:51:46 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@ if ('serviceWorker' in navigator) {
 }
 
 function irParaAbas() {
-  const data = JSON.parse(localStorage.getItem('app')) || { abas: [] };
+  const data = JSON.parse(localStorage.getItem('app')) || {};
+  if (!Array.isArray(data.abas)) {
+    data.abas = [];
+  }
   if (data.abas.length === 0) {
     data.abas.push({ id: Date.now(), nome: 'Minha aba', anotacoes: [] });
     localStorage.setItem('app', JSON.stringify(data));
